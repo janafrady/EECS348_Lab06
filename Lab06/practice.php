079e2477992054a15d7cd5c592e866e3be9e0ad8
janafrady <126286248+janafrady@users.noreply.github.com>
2024-10-25 09:39:17 -0500

    Add files via upload

--- Lab06/practice.php
+++ Lab06/practice.php
@@ -4,12 +4,23 @@ $size = $_GET["size"];
 
 echo "<table border='1'>";
 
+echo "<tr>";
+echo "<th>" . " " . "</th>";
+
+for ($i = 1; $i <= $size; $i++){
+    echo "<th>" . $i . "</th>";
+}
+
+echo "</tr>";
+
 for ($i = 1; $i <= $size; $i++){
     echo "<tr>";
+    echo "<th>" . $i . "</th>";
     for ($j = 1; $j <= $size; $j++){
         echo "<td>" . $i * $j . "</td>";
     }
     echo "</tr>";
 }
+
 echo "</table>";
 ?>
